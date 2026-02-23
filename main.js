492c285b86ace4d8e3b8a20b32c1670bfe0bcf5f
Marik0023 <70592085+Marik0023@users.noreply.github.com>
2026-02-23 11:42:53 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.161.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.161.0/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.161.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.161.0/build/three.module.js?module';
+import { GLTFLoader } from 'https://unpkg.com/three@0.161.0/examples/jsm/loaders/GLTFLoader.js?module';
+import { OrbitControls } from 'https://unpkg.com/three@0.161.0/examples/jsm/controls/OrbitControls.js?module';
 
 const ui = {
   introModal: document.getElementById('introModal'),
